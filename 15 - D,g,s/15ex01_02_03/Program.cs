fdb4bfc4ea431649324100054c77f91a1ab22843
Christian pelo PC <forbiddome@gmail.com>
2022-06-22 11:14:10 -0300

    Dictionary, Generics, Set

--- 15 - D,g,s/15ex01_02_03/Program.cs
+++ 15 - D,g,s/15ex01_02_03/Program.cs
@@ -46,19 +46,86 @@ namespace _15ex01_02_03
                     throw new Exception("Unexpected error: " + e.Message);
                 }
             }
-            */
+            
             //ex02--------------------------------------------------------------------------------------------------
 
             Console.WriteLine("\n\nExercise 02\n");
             {
 
-            }
+                HashSet<int> list = new();
+                Console.Write("How Many Studentes for Course A: ");
+                int a = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= a; i++)
+                {
+                    Console.Write($"Enter the Id of the A course's student #{i}: ");
+                    int id = int.Parse(Console.ReadLine());
+                    list.Add(id);
+                }
+                Console.Write("How Many Studentes for Course B: ");
+                int b = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= b; i++)
+                {
+                    Console.Write($"Enter the Id of the B course's student #{i}: ");
+                    int id = int.Parse(Console.ReadLine());
+                    list.Add(id);
+                }
+                Console.Write("How Many Studentes for Course C: ");
+                int c = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= c; i++)
+                {
+                    Console.Write($"Enter the Id of the C course's student #{i}: ");
+                    int id = int.Parse(Console.ReadLine());
+                    list.Add(id);
+                }
+                // Tipos nativos do System normalmente já implementam o IComparer logo usam padronizadamente o CompareTo
+                Console.Write("Total Students: "+ list.Count);
 
+            }
+            */
             //ex03--------------------------------------------------------------------------------------------------
 
             Console.WriteLine("\n\nExercise 03\n");
             {
+                try
+                {
+                    Console.Write("Enter the votes file's full path: ");
+                    //C:\Users\Paulo\Desktop\Csharp-Completo-Nelio-Alves\15 - D,g,s\in\in.txt
+                    string path = Console.ReadLine();
+                    Dictionary<string, int> lista = new Dictionary<string, int>();
+                    using (StreamReader sr = File.OpenText(path))
+                    {
+                        while (!(sr.EndOfStream))
+                        {
+                            string[] data = sr.ReadLine().Split(",");
+                            string name = data[0];
+                            int votes = int.Parse(data[1]);
+                            if (lista.ContainsKey(name))
+                            {
+                                lista[name] += votes;
+                            }
+                            else
+                            {
+                                lista.Add(name, votes);
+                            }
+                        }
+                    }
+                    //C:\Users\Paulo\Desktop\Csharp-Completo-Nelio-Alves\15 - D,g,s\out\
+                    Console.WriteLine("Enter the votes RELATORY's full path to be created");
+                    path = Console.ReadLine();
+                    Directory.CreateDirectory(path);
 
+                    using (StreamWriter sw = new(path + @"\relatory.csv"))
+                    {
+                        foreach (KeyValuePair<string, int> candidate in lista)
+                        {
+                            sw.WriteLine(candidate.Key + ": " + candidate.Value);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Error: " + e.Message);
+                }
             }
         }
     }
