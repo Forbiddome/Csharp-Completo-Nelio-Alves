20dbc8fa955bad330a959f591fab2157e7eb7214
Christian pelo PC <forbiddome@gmail.com>
2022-06-22 01:30:33 -0300

    Exercícios interfaces pelo PC

--- 14 - Interfaces/14ex01_02_03/Program.cs
+++ 14 - Interfaces/14ex01_02_03/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace _14ex01_02_03
 {
@@ -20,15 +23,48 @@ namespace _14ex01_02_03
 
             Console.WriteLine("\n\nExercise 02\n");
             {
+                string path = @"C:\Users\Paulo\Desktop\Csharp-Completo-Nelio-Alves\14 - Interfaces\Arquive.txt";
+                List<string> lista = new List<string>();
+                try
+                {
+                    string[] arquive = File.ReadAllLines(path);
+                    foreach (string line in arquive) lista.Add(line);
+                    lista.Sort(); // Usa o Metodo Default da Interface IComparer para dar o Sort()
+                    foreach (string name in lista) Console.WriteLine(name);
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Unexpected Exception: " + e.Message);
+                }
 
             }
-
 
             //ex03--------------------------------------------------------------------------------------------------
 
             Console.WriteLine("\n\n\nExercise 03\n");
             {
-
+                string path = @"C:\Users\Paulo\Desktop\Csharp-Completo-Nelio-Alves\14 - Interfaces\Arquive.csv";
+                try
+                {
+                    using (StreamReader sr = File.OpenText(path))
+                    {
+                        List<EmployeeData> lista = new List<EmployeeData>();
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            string[] data = line.Split(",");
+                            string name = data[0];
+                            double salary = double.Parse(data[1], CultureInfo.InvariantCulture);
+                            lista.Add(new EmployeeData { Name = name, Salary = salary });
+                        }
+                        lista.Sort(); ;
+                        foreach (EmployeeData employeeData in lista) Console.WriteLine(employeeData);
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Unexpected Exception: " + e.Message);
+                }
             }
         }
     }
