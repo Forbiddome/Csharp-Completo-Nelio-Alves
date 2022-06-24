8db17ec76da8b2af414533085ff167b16e97ebe2
Christian <forbiddome@gmail.com>
2022-06-24 10:59:47 -0300

    LINQ, inicio da atividade

---
+++ 17 - Lambda, LINQ, Delegates/17ex01_02_03_04_05_06/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _17ex01_02_03_04_05_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
